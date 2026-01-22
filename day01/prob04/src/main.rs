47df9855a0f37b03a48dc9b5a9fa34d69d7ca0f7
NogiNogio <naokiogino3e8@gmail.com>
2026-01-22 18:12:02 +0000

    chore: add probrem files

---
+++ day01/prob04/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let a = 9;
+    let b = 4;
+
+    println!("{a} + {b} = {}", a+b);
+    println!("{a} - {b} = {}", a-b);
+    println!("{a} * {b} = {}", a*b);
+    println!("{a} / {b} = {}", a/b);
+    println!("{a} % {b} = {}", a%b);
+}
